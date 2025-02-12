e98eb460d6580e1e85a9bb7921ce8b82ad543f26
baeumesindbaumig <166114518+baeumesindbaumig@users.noreply.github.com>
2025-02-12 18:07:30 +0100

    [ADDED] mehr Objekte und Licht

--- godot_abschlussprojekt_lena_würbach/camera_controller.gd
+++ godot_abschlussprojekt_lena_würbach/camera_controller.gd
@@ -25,3 +25,29 @@
 	# Kamera um die Y-Achse drehen
 	#rotate_y(input * rotation_speed * delta)
 	
+	#--------------------------------------------------
+extends Node3D
+
+@onready var raycast = $Camera3D/RayCast3D  # Pfad zum RayCast
+@onready var fridge_door = $"../Kühlschrank/Kühlschrank_Tür"  # Die Tür-Node
+
+var door_open = false  # Zustand der Tür
+var open_rotation = deg_to_rad(100)  # Wie weit sich die Tür öffnet
+var closed_rotation = deg_to_rad(0)  # Geschlossene Position
+
+func _input(event):
+	if event.is_action_pressed("interact") and raycast.is_colliding():
+		var target = raycast.get_collider()  # Das Objekt, auf das der RayCast zeigt
+		
+		if target.name == "Tür":  # Prüft, ob es die Kühlschranktür ist
+			toggle_door()
+
+func toggle_door():
+	door_open = !door_open  # Tür-Zustand wechseln
+	var target_rotation = open_rotation if door_open else closed_rotation
+	
+	# Die Tür mit einer kleinen Animation bewegen
+	var tween = get_tree().create_tween()
+	tween.tween_property(fridge_door, "rotation:y", target_rotation, 0.5)
+
+	
---
+++ godot_abschlussprojekt_lena_würbach/kühlschrank_tür_oben.gd
@@ -0,0 +1,31 @@
+#extends MeshInstance3D
+
+# Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
+
+
+extends MeshInstance3D
+
+@export var open_rotation = deg_to_rad(100)  # Wie weit sich die Tür öffnet
+@export var closed_rotation = deg_to_rad(0)  # Winkel der geschlossenen Tür
+
+var door_open = false  # Tür-Zustand
+
+func _input(event):
+	print("E wurde gedrückt!")
+	if event.is_action_pressed("interact"):  # "E" drücken
+		toggle_door()
+
+func toggle_door():
+	door_open = !door_open  # Zustand wechseln
+	var target_rotation = open_rotation if door_open else closed_rotation
+	
+	# Animierte Rotation
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "rotation:y", target_rotation, 0.5)
